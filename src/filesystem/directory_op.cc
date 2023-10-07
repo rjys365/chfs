2d95dbc1973b9f909d3a76b65878a592190c46a4
Zhang Zhuyue <zhang.zhuyue@sjtu.edu.cn>
2023-10-07 05:37:46 +0000

    Part 3C: operations on directory entries

--- src/filesystem/directory_op.cc
+++ src/filesystem/directory_op.cc
@@ -40,9 +40,8 @@ auto dir_list_to_string(const std::list<DirectoryEntry> &entries)
 auto append_to_directory(std::string src, std::string filename, inode_id_t id)
     -> std::string {
 
-  // TODO: Implement this function.
-  //       Append the new directory entry to `src`.
-  UNIMPLEMENTED();
+  // Append the new directory entry to `src`.
+  src+=("/"+filename+":"+std::to_string(id));
   
   return src;
 }
@@ -50,8 +49,23 @@ auto append_to_directory(std::string src, std::string filename, inode_id_t id)
 // {Your code here}
 void parse_directory(std::string &src, std::list<DirectoryEntry> &list) {
 
-  // TODO: Implement this function.
-  UNIMPLEMENTED();
+  list.clear();
+  std::size_t pos=0;
+  if(src[0]=='/')pos++;
+  while(true){
+    std::size_t colon_pos=src.find(':',pos);
+    if(colon_pos==std::string::npos)return;
+    std::string name=src.substr(pos,colon_pos-pos);
+    pos=colon_pos+1;
+    std::size_t slash_pos=src.find('/',pos);
+    std::string inode_id_str;
+    if(slash_pos==std::string::npos)inode_id_str=src.substr(pos);
+    else inode_id_str=src.substr(pos,slash_pos-pos);
+    inode_id_t inode_id=std::stoull(inode_id_str);
+    list.push_back((DirectoryEntry){std::move(name),inode_id});
+    if(slash_pos==std::string::npos)return;
+    pos=slash_pos+1;
+  }
 
 }
 
@@ -60,9 +74,29 @@ auto rm_from_directory(std::string src, std::string filename) -> std::string {
 
   auto res = std::string("");
 
-  // TODO: Implement this function.
-  //       Remove the directory entry from `src`.
-  UNIMPLEMENTED();
+  // Remove the directory entry from `src`.
+  std::size_t pos=0;
+  if(src[0]=='/')pos++;
+  bool first_file=true;
+  while(true){
+    std::size_t colon_pos=src.find(':',pos);
+    if(colon_pos==std::string::npos)break;
+    std::string name=src.substr(pos,colon_pos-pos);
+    pos=colon_pos+1;
+    std::size_t slash_pos=src.find('/',pos);
+    std::string inode_id_str;
+    if(slash_pos==std::string::npos)inode_id_str=src.substr(pos);
+    else inode_id_str=src.substr(pos,slash_pos-pos);
+    if(name!=filename){
+      if(first_file){
+        res=name+":"+inode_id_str;
+        first_file=false;
+      }
+      else res+="/"+name+":"+inode_id_str;
+    }
+    if(slash_pos==std::string::npos)break;
+    pos=slash_pos+1;
+  }
 
   return res;
 }
@@ -73,8 +107,13 @@ auto rm_from_directory(std::string src, std::string filename) -> std::string {
 auto read_directory(FileOperation *fs, inode_id_t id,
                     std::list<DirectoryEntry> &list) -> ChfsNullResult {
   
-  // TODO: Implement this function.
-  UNIMPLEMENTED();
+  auto read_res=fs->read_file(id);
+  if(read_res.is_err()){
+    return ChfsNullResult(read_res.unwrap_error());
+  }
+  auto data=read_res.unwrap();
+  auto data_str=std::string(data.begin(),data.end());
+  parse_directory(data_str,list);
 
   return KNullOk;
 }
