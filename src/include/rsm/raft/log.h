b3e0363ff84bd82fe62bce4183dd1af52e3210ab
Zhang Zhuyue <zhang.zhuyue@sjtu.edu.cn>
2023-12-03 05:27:53 +0000

    start to work on lab3 and format code

--- src/include/rsm/raft/log.h
+++ src/include/rsm/raft/log.h
@@ -1,41 +1,39 @@
 #pragma once
 
-#include "common/macros.h"
-#include "block/manager.h"
+#include <cstring>
 #include <mutex>
 #include <vector>
-#include <cstring>
+
+#include "block/manager.h"
+#include "common/macros.h"
 
 namespace chfs {
 
-/** 
+/**
  * RaftLog uses a BlockManager to manage the data..
  */
 template <typename Command>
 class RaftLog {
-public:
-    RaftLog(std::shared_ptr<BlockManager> bm);
-    ~RaftLog();
+ public:
+  RaftLog(std::shared_ptr<BlockManager> bm);
+  ~RaftLog();
 
-    /* Lab3: Your code here */
+  /* Lab3: Your code here */
 
-private:
-    std::shared_ptr<BlockManager> bm_;
-    std::mutex mtx;
-    /* Lab3: Your code here */
-
+ private:
+  std::shared_ptr<BlockManager> bm_;
+  std::mutex mtx;
+  /* Lab3: Your code here */
 };
 
 template <typename Command>
-RaftLog<Command>::RaftLog(std::shared_ptr<BlockManager> bm)
-{
-    /* Lab3: Your code here */
+RaftLog<Command>::RaftLog(std::shared_ptr<BlockManager> bm) {
+  /* Lab3: Your code here */
 }
 
 template <typename Command>
-RaftLog<Command>::~RaftLog()
-{
-    /* Lab3: Your code here */
+RaftLog<Command>::~RaftLog() {
+  /* Lab3: Your code here */
 }
 
 /* Lab3: Your code here */
--- src/include/rsm/raft/protocol.h
+++ src/include/rsm/raft/protocol.h
@@ -1,7 +1,7 @@
 #pragma once
 
-#include "rsm/raft/log.h"
 #include "rpc/msgpack.hpp"
+#include "rsm/raft/log.h"
 
 namespace chfs {
 
@@ -18,70 +18,69 @@ const std::string RAFT_RPC_APPEND_ENTRY = "append entries";
 const std::string RAFT_RPC_INSTALL_SNAPSHOT = "install snapshot";
 
 struct RequestVoteArgs {
-    /* Lab3: Your code here */
-    
-    MSGPACK_DEFINE(
-    
-    )
+  /* Lab3: Your code here */
+  int term, candidate_id, last_log_index, last_log_term;
+
+  MSGPACK_DEFINE(term, candidate_id, last_log_index, last_log_term)
 };
 
 struct RequestVoteReply {
-    /* Lab3: Your code here */
+  /* Lab3: Your code here */
+  int term;
+  bool vote_granted;
 
-    MSGPACK_DEFINE(
-    
-    )
+  MSGPACK_DEFINE(term, vote_granted)
 };
 
 template <typename Command>
 struct AppendEntriesArgs {
-    /* Lab3: Your code here */
+  /* Lab3: Your code here */
 };
 
 struct RpcAppendEntriesArgs {
-    /* Lab3: Your code here */
+  /* Lab3: Your code here */
 
-    MSGPACK_DEFINE(
-    
-    )
+  MSGPACK_DEFINE(
+
+  )
 };
 
 template <typename Command>
-RpcAppendEntriesArgs transform_append_entries_args(const AppendEntriesArgs<Command> &arg)
-{
-    /* Lab3: Your code here */
-    return RpcAppendEntriesArgs();
+RpcAppendEntriesArgs transform_append_entries_args(
+    const AppendEntriesArgs<Command> &arg) {
+  /* Lab3: Your code here */
+  return RpcAppendEntriesArgs();
 }
 
 template <typename Command>
-AppendEntriesArgs<Command> transform_rpc_append_entries_args(const RpcAppendEntriesArgs &rpc_arg)
-{
-    /* Lab3: Your code here */
-    return AppendEntriesArgs<Command>();
+AppendEntriesArgs<Command> transform_rpc_append_entries_args(
+    const RpcAppendEntriesArgs &rpc_arg) {
+  /* Lab3: Your code here */
+  return AppendEntriesArgs<Command>();
 }
 
 struct AppendEntriesReply {
-    /* Lab3: Your code here */
+  /* Lab3: Your code here */
 
-    MSGPACK_DEFINE(
-    
-    )
+  MSGPACK_DEFINE(
+
+  )
 };
 
 struct InstallSnapshotArgs {
-    /* Lab3: Your code here */
+  /* Lab3: Your code here */
 
-    MSGPACK_DEFINE(
-    
-    )
+  MSGPACK_DEFINE(
+
+  )
 };
 
 struct InstallSnapshotReply {
-    /* Lab3: Your code here */
+  /* Lab3: Your code here */
 
-    MSGPACK_DEFINE(
-    
-    )
+  MSGPACK_DEFINE(
+
+  )
 };
 
 } /* namespace chfs */
